11dfe8bbcfa03b51d7c669918d781e1b97b5c10f
Tomas Fransoi <tomasfransoi@gmail.com>
2023-10-19 21:45:53 -0300

    sinprop1.0.1

--- src/Components/Lista/Lista.js
+++ src/Components/Lista/Lista.js
@@ -9,37 +9,37 @@ const Lista = () =>{
                 <h3>Vicepresidente: Garay Martino</h3>
             </article>
             <article>
-                <h2>Secretaria de gestión</h2>
+                <h2>Secretaría de Gestión</h2>
                 <h3>Titular: Alvarez Candelaria</h3>
                 <h3>Suplente: Mile Verdiani</h3>
             </article>
             <article>
-                <h2>Secretaria de humanidades</h2>
+                <h2>Secretaría de Humanidades</h2>
                 <h3>Titular: Rabadan Antonella</h3>
                 <h3>Suplente: Carballo Guadalupe</h3>
             </article>
             <article>
-                <h2>Secretaria de naturales</h2>
+                <h2>Secretaría de Naturales</h2>
                 <h3>Titular: Fransoi Tomas</h3>
                 <h3>Suplente: Oviedo Sofia</h3>
             </article>
             <article>
-                <h2>Secretaria de educación física</h2>
+                <h2>Secretaría de Educación Física</h2>
                 <h3>Titular: Nobili Camila</h3>
                 <h3>Suplente: Canovai Genaro</h3>
             </article>
             <article>
-                <h2>Prensa y difusión</h2>
+                <h2>Prensa y Difusión</h2>
                 <h3>Titular: Alarcón Antonella</h3>
                 <h3>Suplente: Julián Onofrio</h3>
             </article>
             <article>
-                <h2>Asuntos estudiantiles</h2>
+                <h2>Asuntos Estudiantiles</h2>
                 <h3>Titular: Candelaria Oberti</h3>
                 <h3>suplente: Barragán Candela</h3>
             </article>
             <article>
-                <h2>secretaria de finanzas</h2>
+                <h2>Secretaría de Finanzas</h2>
                 <h3>Titular: Parano Julia</h3>
                 <h3>Suplente: Samira Daer</h3>
             </article>
